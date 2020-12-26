df2f2cb65951b8888a790da446cd621a7d5a0c94
rishi-kan <73443819+rishi-kan@users.noreply.github.com>
2020-12-26 15:44:23 -0500

    Add files via upload

---
+++ Xcode Tester/Xcode Tester/Xcode_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_TesterApp.swift
+//  Xcode Tester
+//
+//  Created by Rishi Kandagadla on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
